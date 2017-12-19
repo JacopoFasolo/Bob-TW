5a97e8610499aef31c040f1b3247e36749f431ac
aliceinland <carlotta.segna@studenti.unipd.it>
2017-12-19 18:26:21 +0100

    Inserito il font + sistemato il db + fatto cose

--- implementazionedatabase.sql
+++ implementazionedatabase.sql
@@ -4,15 +4,15 @@ Username char(16) primary key,
 Email varchar(50) NOT NULL,
 Password varchar(20) NOT NULL,
 Cognome varchar(50) NOT NULL,
-Sesso enum("M","F") NOT NULL,
+Sesso enum('M','F') NOT NULL,
 Data_Nascita date NOT NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE Biglietti
 (
 Id_biglietto int AUTO_INCREMENT primary key,
-Durata enum("Giornaliero","Settimanale","Bisettimanale","Dieciingressi","Mezzastagione","Stagionale") NOT NULL,
-Tipo enum("Interoas","Interobs","Ridottoas","Ridottobs") NOT NULL,
+Durata enum('Giornaliero','Settimanale','Bisettimanale','Dieciingressi','Mezzastagione','Stagionale') NOT NULL,
+Tipo enum('Interoas','Interobs','Ridottoas','Ridottobs') NOT NULL,
 Prezzo double NOT NULL,
 Data_Inizio date NOT NULL,
 Data_Fine date NOT NULL

e3596f2f6e0900375d9227e76eab4dedd4238aee
Garoth93 <ipod1993@gmail.com>
2017-12-19 15:58:47 +0100

    asd
    asd

--- implementazionedatabase.sql
+++ implementazionedatabase.sql
@@ -18,21 +18,84 @@ Data_Inizio date NOT NULL,
 Data_Fine date NOT NULL
 )ENGINE=InnoDB;
 
-CREATE TABLE Attrezzatura
+CREATE TABLE Id_oggetti
 (
+Id_oggetto int AUTO_INCREMENT primary key
 )ENGINE=InnoDB;
 
-CREATE TABLE Attrezzaturanoleggio
+CREATE TABLE Sci
 (
-)ENGINE=InnoDB;
-.
+Id_s int,
+Prezzo double NOT NULL,
+Marca char(16) NOT NULL,
+FOREIGN KEY (Id_s) REFERENCES Id_oggetti(Id_oggetto)
+ 	ON DELETE CASCADE
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
+
+CREATE TABLE Piccozze
+(
+Id_p int,
+Prezzo double NOT NULL,
+Marca char(16) NOT NULL,
+FOREIGN KEY (Id_p) REFERENCES Id_oggetti(Id_oggetto)
+ 	ON DELETE CASCADE
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
+
+CREATE TABLE Ciaspole
+(
+Id_c int,
+Prezzo double NOT NULL,
+Marca char(16) NOT NULL,
+FOREIGN KEY (Id_c) REFERENCES Id_oggetti(Id_oggetto)
+ 	ON DELETE CASCADE
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
+
+CREATE TABLE Scarponi
+(
+Id_sc int,
+Prezzo double NOT NULL,
+Marca char(16) NOT NULL,
+FOREIGN KEY (Id_sc) REFERENCES Id_oggetti(Id_oggetto)
+ 	ON DELETE CASCADE
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
+
+CREATE TABLE Caschi
+(
+Id_ca int,
+Prezzo double NOT NULL,
+Marca char(16) NOT NULL,
+FOREIGN KEY (Id_ca) REFERENCES Id_oggetti(Id_oggetto)
+ 	ON DELETE CASCADE
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
+
+CREATE TABLE Snowboard
+(
+Id_sn int,
+Prezzo double NOT NULL,
+Marca char(16) NOT NULL,
+FOREIGN KEY (Id_sn) REFERENCES Id_oggetti(Id_oggetto)
+ 	ON DELETE CASCADE
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
 
 CREATE TABLE Commento
 (
 Id_commento int AUTO_INCREMENT primary key,
-Testo memo,
-)ENGINE=InnoDB;
+Testo varchar(255) NOT NULL
+) ENGINE=InnoDB;
 
-CREATE TABLE Commentoins
+CREATE TABLE Commento_ins
 (
+Id_com int,
+id_ogg int,
+FOREIGN KEY (Id_com) REFERENCES Commento(Id_commento),
+FOREIGN KEY (Id_ogg) REFERENCES Id_oggetti(Id_oggetto),
+ 	ON DELETE CASCADE
+    ON UPDATE NO ACTION
+    
 )ENGINE=InnoDB;

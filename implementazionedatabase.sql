e02e6a0655707cbd5c43c427fd9d68df13aaf7d3
Garoth93 <ipod1993@gmail.com>
2017-12-21 11:10:31 +0100

    ADD_MINNE
    ADD_MINNE

--- implementazionedatabase.sql
+++ implementazionedatabase.sql
@@ -1,14 +1,14 @@
-DROP TABLE IF EXIST Commento_ins;
-DROP TABLE IF EXIST Commento;
-DROP TABLE IF EXIST Snowboard;
-DROP TABLE IF EXIST Caschi;
-DROP TABLE IF EXIST Scarponi;
-DROP TABLE IF EXIST Ciaspole;
-DROP TABLE IF EXIST Piccozze;
-DROP TABLE IF EXIST Sci;
-DROP TABLE IF EXIST Id_oggetti;
-DROP TABLE IF EXIST Biglietti;
-DROP TABLE IF EXIST Utente;
+DROP TABLE IF EXISTS Commento_ins;
+DROP TABLE IF EXISTS Commento;
+DROP TABLE IF EXISTS Snowboard;
+DROP TABLE IF EXISTS Caschi;
+DROP TABLE IF EXISTS Scarponi;
+DROP TABLE IF EXISTS Ciaspole;
+DROP TABLE IF EXISTS Piccozze;
+DROP TABLE IF EXISTS Sci;
+DROP TABLE IF EXISTS Id_oggetti;
+DROP TABLE IF EXISTS Biglietti;
+DROP TABLE IF EXISTS Utente;
 
 CREATE TABLE Utente
 (
@@ -16,6 +16,7 @@ Id_utente int AUTO_INCREMENT primary key,
 Username char(16) NOT NULL,
 Email varchar(50) NOT NULL,
 Password varchar(20) NOT NULL,
+Nome varchar(50) NOT NULL,
 Cognome varchar(50) NOT NULL,
 Sesso enum('M','F') NOT NULL,
 Data_Nascita date NOT NULL
@@ -42,8 +43,8 @@ Id_s int,
 Prezzo double NOT NULL,
 Marca char(16) NOT NULL,
 FOREIGN KEY (Id_s) REFERENCES Id_oggetti(Id_oggetto)
- 	ON DELETE CASCADE
-    ON UPDATE NO ACTION
+ON DELETE CASCADE
+ON UPDATE NO ACTION
 ) ENGINE=InnoDB;
 
 CREATE TABLE Piccozze
@@ -52,8 +53,8 @@ Id_p int,
 Prezzo double NOT NULL,
 Marca char(16) NOT NULL,
 FOREIGN KEY (Id_p) REFERENCES Id_oggetti(Id_oggetto)
- 	ON DELETE CASCADE
-    ON UPDATE NO ACTION
+ON DELETE CASCADE
+ON UPDATE NO ACTION
 ) ENGINE=InnoDB;
 
 CREATE TABLE Ciaspole
@@ -62,8 +63,8 @@ Id_c int,
 Prezzo double NOT NULL,
 Marca char(16) NOT NULL,
 FOREIGN KEY (Id_c) REFERENCES Id_oggetti(Id_oggetto)
- 	ON DELETE CASCADE
-    ON UPDATE NO ACTION
+ON DELETE CASCADE
+ON UPDATE NO ACTION
 ) ENGINE=InnoDB;
 
 CREATE TABLE Scarponi
@@ -72,8 +73,8 @@ Id_sc int,
 Prezzo double NOT NULL,
 Marca char(16) NOT NULL,
 FOREIGN KEY (Id_sc) REFERENCES Id_oggetti(Id_oggetto)
- 	ON DELETE CASCADE
-    ON UPDATE NO ACTION
+ON DELETE CASCADE
+ON UPDATE NO ACTION
 ) ENGINE=InnoDB;
 
 CREATE TABLE Caschi
@@ -82,8 +83,8 @@ Id_ca int,
 Prezzo double NOT NULL,
 Marca char(16) NOT NULL,
 FOREIGN KEY (Id_ca) REFERENCES Id_oggetti(Id_oggetto)
- 	ON DELETE CASCADE
-    ON UPDATE NO ACTION
+ON DELETE CASCADE
+ON UPDATE NO ACTION
 ) ENGINE=InnoDB;
 
 CREATE TABLE Snowboard
@@ -92,8 +93,8 @@ Id_sn int,
 Prezzo double NOT NULL,
 Marca char(16) NOT NULL,
 FOREIGN KEY (Id_sn) REFERENCES Id_oggetti(Id_oggetto)
- 	ON DELETE CASCADE
-    ON UPDATE NO ACTION
+ON DELETE CASCADE
+ON UPDATE NO ACTION
 ) ENGINE=InnoDB;
 
 CREATE TABLE Commento
@@ -109,8 +110,10 @@ Id_ogg int,
 Id_ut int,
 FOREIGN KEY (Id_com) REFERENCES Commento(Id_commento),
 FOREIGN KEY (Id_ogg) REFERENCES Id_oggetti(Id_oggetto),
-FOREIGN KEY (Id_ut) REFERENCES Utente(Id_utente),
- 	ON DELETE CASCADE
-    ON UPDATE NO ACTION
-    
+FOREIGN KEY (Id_ut) REFERENCES Utente(Id_utente)
+ON DELETE CASCADE
+ON UPDATE NO ACTION  
 )ENGINE=InnoDB;
+
+INSERT INTO Utente (Username, Email, Password, Nome, Cognome, Sesso, Data_Nascita)
+VALUES ('BOB','bob@bob.bob','bob','Bob','Bob','M','1993-12-25');

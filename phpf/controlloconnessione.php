e0e35495b53990eee27b4fb7917b47a29b766e08
Garoth93 <ipod1993@gmail.com>
2017-12-22 12:57:27 +0100

    a
    a

--- phpf/controlloconnessione.php
+++ phpf/controlloconnessione.php
@@ -6,9 +6,11 @@
     $query = "select * from utente where Username='".$uname."' and Password='".$upassword."'"; //la query per controllare se esiste l'utente nel db
     $result = mysqli_query($conn,$query); //eseguo la query
     $count = mysqli_num_rows($result); //variabile count contiene numero di righe ricevute come risultato
+    $idutente = mysqli_fetch_array($result);//serve per ricavare poi il risultato della query, in questo caso serve per mantenere id dell'utente che effettua il login
     if($count>0){ //controllo se esiste un utente valido corrispondente ai valori inseriti nella form, esiste se e solo se count è maggiore di 0 
-        $_SESSION["login"]="1";
-        $_SESSION["USER"]=$uname;//se esiste creo la variabile di tipo _SESSION chiamato login 
+        $_SESSION["login"]="1";//crea una var session globale per vedere se c'è login
+        $_SESSION["USER"]=$uname;//creo var session globale per mantenere il nome utente
+        $_SESSION["IDUSER"]=$idutente['Id_utente'];//crea var session globale per mantenere Id_utente
         header("location:../index.html"); //utente valido
     } else {
         header("location:login.php?err=1");//qualcosa è andato storto, riptovare! Torno nella pagina di login.
--- phpf/prova.php
+++ phpf/prova.php
@@ -1,8 +1,13 @@
 <?php
 	session_start();
-	if(!isset($_SESSION["login"])){
-	}else{
+	if(!isset($_SESSION["login"])){ // non loggato
+
+	}else{ //loggato
+
 		echo $_SESSION["USER"];
+		echo " ";
+		echo $_SESSION["IDUSER"];
+		
 	}
    
 ?>

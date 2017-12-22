67f420ad56675ac916e5e7ba6b878a1a2d7fcaa8
Garoth93 <ipod1993@gmail.com>
2017-12-22 12:20:34 +0100

    log
    log

--- phpf/Login.php
+++ phpf/Login.php
@@ -1,4 +1,27 @@
 <?php
 	echo file_get_contents("../Pezzi di pagina/header_login.html");
+?>
+
+<div>
+    <form action="controlloconnessione.php" method="post" >
+        <div>
+            <label><b>Username:</b></label>
+            <input type="text" name="uname" id="uname" placeholder="nome utente"/>
+            <label><b>Password</b></label>
+            <input type="password" name="upassword" id="upassword" placeholder="**********" />
+            <button type="submit" class="btn btn-primary">Login</button>
+        </div>
+        <div id="err">
+
+            <?php
+               if(isset($_REQUEST["err"]))
+                    echo "Nome utente o password non valido!";
+            ?>
+
+        </div>
+    </form>
+</div>
+
+<?php
 	echo file_get_contents("../Pezzi di pagina/footer.html");
 ?>
--- phpf/controlloconnessione.php
+++ phpf/controlloconnessione.php
@@ -1,18 +1,16 @@
 <?php
 	session_start(); // apro una nuova sessione
 	require "connessione.php"; //file per la connessione al server
-    $uname = $_POST["uname"];  //imposto la variabile uname con il valore di nome utente ricevoto dal form 
-    $upassword = $_POST["upassword"]; //stesso discroso di uname
-    $s = "select * from utente where Username='".$uname."' and Password='".$upassword."'"; //la query per controllare se esiste l'utente nel db
-    $result = mysqli_query($conn,$s); //eseguo la query
+    $uname = $_POST["uname"];  //imposto le variabili ricevute dal form 
+    $upassword = $_POST["upassword"];
+    $query = "select * from utente where Username='".$uname."' and Password='".$upassword."'"; //la query per controllare se esiste l'utente nel db
+    $result = mysqli_query($conn,$query); //eseguo la query
     $count = mysqli_num_rows($result); //variabile count contiene numero di righe ricevute come risultato
-    if($count>0) //controllo se esiste un utente valido corrispondente ai valori inseriti nella form, esiste se e solo se count è maggiore di 0 
-    {
+    if($count>0){ //controllo se esiste un utente valido corrispondente ai valori inseriti nella form, esiste se e solo se count è maggiore di 0 
         $_SESSION["login"]="1";
         $_SESSION["USER"]=$uname;//se esiste creo la variabile di tipo _SESSION chiamato login 
-        header("location:Home.php"); //utente valido
-    else
-    {
-        header("location:Login.php?err=1");//qualcosa è andato storto, riptovare! Torno nella pagina di login.
+        header("location:../index.html"); //utente valido
+    } else {
+        header("location:login.php?err=1");//qualcosa è andato storto, riptovare! Torno nella pagina di login.
     }
 ?>

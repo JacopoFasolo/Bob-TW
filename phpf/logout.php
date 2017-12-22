134daeb319f58253a4a9ed71c3c2d390ceac709b
Garoth93 <ipod1993@gmail.com>
2017-12-22 15:38:44 +0100

    a
    a

--- phpf/logout.php
+++ phpf/logout.php
@@ -2,6 +2,7 @@
 	session_start(); 
 	unset($_SESSION["login"]);
     unset($_SESSION["USER"]);//cancello la variabile login
-    unset($_SESSION["IDUSER"]); 
+    unset($_SESSION["IDUSER"]);
+    //aggiungere session destroy
     header("location:../index.html") //ritorno all pagina di login 
 ?>

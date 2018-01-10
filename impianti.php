c01dfe043d908f557c9a293f2fad8b1f38bf43f6
JacopoFasolo <jacopo.fasolo.1@studenti.unipd.it>
2018-01-10 21:43:45 +0100

    modificato tutti i php per caricare le nuove parti di html

--- impianti.php
+++ impianti.php
@@ -1,6 +1,28 @@
+<!-- Prova caricamento pagina princale -->
 <?php
-    $pag= 'pag';
-    echo file_get_contents("Pezzi_di_pagina/header_login.html");
+    $titolo = "Bob - I nostri impianti";
+
+    #importo il doctype e l'head della pagina
+    echo file_get_contents("Pezzi_di_pagina/doctype(modificare).html");
+    
+	#inizio del tag body della pagina
+    echo "<body>";
+
+    #controllo se utente loggato cosi da far vedere la triscia superiore di login o la striscia superiore ciao utente se già loggato
+    if(!isset($_SESSION["login"]))
+    	echo file_get_contents("Pezzi_di_pagina/login.html");
+    else
+    	echo file_get_contents("Pezzi_di_pagina/loggedin.html");
+    
+    #includo header della pagina
+    echo file_get_contents("Pezzi_di_pagina/header.html");
+    
+    #includo il menu e middle della pagina
     echo file_get_contents("html/impianti.html");
+
+    #includo il footer
     echo file_get_contents("Pezzi_di_pagina/footer.html");
+    
+    #chiudo tags aperti prima
+    echo "</body></html>";
 ?>

29ceea1258448ed46221e319a8bb45451409d466
izanetti <33939535+izanetti@users.noreply.github.com>
2018-01-11 11:42:38 +0100

    modifiche
    queste cose sono poi da sistemare

---
+++ phpf/provastampasci.php
@@ -0,0 +1,25 @@
+<?php
+	require "connessione.php";
+	echo file_get_contents("../Pezzi_di_pagina/header_login.html");
+?>
+<div id="tabsci">
+
+	<table>
+  		<th>SCI </th>
+  		<tr>
+    	<td>Prezzo</td>
+    	<td>Marca</td>    
+  		</tr>
+
+ 			<?php
+ 				$sql = "SELECT Prezzo, Marca FROM sci";
+ 				$result = $conn->query($sql);
+ 				foreach ($result as $asd) {
+ 					echo '<tr>';
+					echo '<td>'; print $asd["Prezzo"]; echo '</td>' ;
+					echo '<td>'; print $asd["Marca"]; echo '</td>';
+					echo '</tr>';
+ 				}
+?>
+	</table>
+</div>

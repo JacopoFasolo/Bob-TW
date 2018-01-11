6f736e71562a4729dce9fca0b5059587acebcad3
Unknown <carlotta.segna@studenti.unipd.it>
2018-01-11 12:43:07 +0100

    Auto stash before merge of "master" and "origin/master"
    ufudhshi

--- registration.php
+++ registration.php
@@ -2,7 +2,7 @@
 session_start(); // dive essere la prima cosa nella pagina, aprire la sessione
 include "db_con.php"; // includo il file di connessione al database
 if($_POST["username_reg"] != "" && $_POST["password_reg"]!= "" && $_POST["email_reg"] != ""){  // se i parametri iscritto non sono vuoti non sono vuote
-$query_registrazione = mysql_query("INSERT INTO Utente (Username,Password,Email)
+$query_registrazione = mysql_query("INSERT INTO utente (Username,Password,Email)
 VALUES ('".$_POST["username_reg"]."','".$_POST["password_reg"]."','".$_POST["email_reg"]."')") // scrivo sul DB questi valori
 or die ("query di registrazione non riuscita".mysql_error()); // se la query fallisce mostrami questo errore
 }else{

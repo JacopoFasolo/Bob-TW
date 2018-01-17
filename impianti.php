0c91522005c3443866bc72b321851c6985e318f2
aliceinland <carlotta.segna@studenti.unipd.it>
2018-01-17 19:08:46 +0100

    lalala
    lalala land

--- impianti.php
+++ impianti.php
@@ -1,9 +1,29 @@
+<!-- Prova caricamento pagina princale -->
 <?php
+    $titolo = "Bob - Impianti";
 
-    $pag= 'pag';
-    echo file_get_contents("Pezzi_di_pagina/header_login.html");
+    #importo il doctype e l'head della pagina
+    echo file_get_contents("Pezzi_di_pagina/doctype(modificare).html");
+    
+	#inizio del tag body della pagina
+    echo "<body>";
+
+    #controllo se utente loggato cosi da far vedere la triscia superiore di login o la striscia superiore ciao utente se già loggato
+    if(!isset($_SESSION["login"]))
+    	echo file_get_contents("Pezzi_di_pagina/login.html");
+    else
+    	echo file_get_contents("Pezzi_di_pagina/loggedin.html");
+    
+    #includo header della pagina
+    echo file_get_contents("Pezzi_di_pagina/header.html");
+    
+    #includo il menu e middle della pagina
     echo file_get_contents("html/impianti.html");
 
+    #includo il footer
     echo file_get_contents("Pezzi_di_pagina/footer.html");
+    
+    #chiudo tags aperti prima
+    echo "</body></html>";
 ?>
 
--- login.php
+++ login.php
@@ -1,9 +1,23 @@
-
-
+<!-- Prova caricamento pagina princale -->
 <?php
-    $pag= 'pag';
-    echo file_get_contents("Pezzi_di_pagina/header_login.html");
+    $titolo = "Bob - Login";
+
+    #importo il doctype e l'head della pagina
+    echo file_get_contents("Pezzi_di_pagina/doctype(modificare).html");
+    
+	#inizio del tag body della pagina
+    echo "<body>";
+    
+    #includo header della pagina
+    echo file_get_contents("Pezzi_di_pagina/header.html");
+    
+    #includo il menu e middle della pagina
     echo file_get_contents("html/login.html");
+
+    #includo il footer
     echo file_get_contents("Pezzi_di_pagina/footer.html");
+    
+    #chiudo tags aperti prima
+    echo "</body></html>";
 ?>
 

022ba2a6d57536108e6efb157808ac5609f979f6
Unknown <jacopo.fasolo.1@studenti.unipd.it>
2018-02-11 22:33:25 +0100

    Auto stash before merge of "master" and "origin/master"

--- php/vissci.php
+++ php/vissci.php
@@ -39,49 +39,55 @@
  	$risquerycercacommento = $conn->query($sql);
  	$contacommentoutente = mysqli_num_rows($risquerycercacommento);
  	$arraydaticommento = mysqli_fetch_array($risquerycercacommento);
-?>
-<div class="prodottonoleggio">
-	<img src="<?php echo $tit["Url_immagine"]; ?>"/>
-    <h1> <?php print $tit["Marca"]; echo " "; print $tit["Modello"]; ?> </h1>
-    <p><?php echo $tit["Descrizione"]; ?></p>
-    <p class ="prezzoprodotto">&euro; <?php print $tit["Prezzo"]; ?> </p>
-    <br/>
-    <?php
-    echo '<p>Ci sono ',$count,' commenti:</p>';
-    if($count > 0){//ho almeno un commento
-		foreach ($res as $com) {
-			if($com["Id_ut"] != $id_utente){//se il commento non è del mio utente lo stampo
-				print $com["Testo"];
-					echo '<br/>';
+	?>
+	<div class="prodottonoleggio">
+		<img src="<?php echo $tit["Url_immagine"]; ?>"/>
+    	<h1> <?php print $tit["Marca"]; echo " "; print $tit["Modello"]; ?> </h1>
+    	<p><?php echo $tit["Descrizione"]; ?></p>
+    	<p class ="prezzoprodotto">&euro; <?php print $tit["Prezzo"]; ?> </p>
+    	<br/>
+	</div>
+	<div id="sezionecommenti">
+    	<?php
+    	if($count > 0){//ho almeno un commento
+    		echo 'Ci sono ',$count,' commenti:';
+			foreach ($res as $com) {
+				if($com["Id_ut"] != $id_utente){//se il commento non è del mio utente lo stampo
+					echo '<p class="commentoaltrui"> &#9749;';
+					print $com["Testo"];
+					echo '</p>';
 				}
 			}
-	}else{//non ho nessun commento
+		}else{//non ho nessun commento
 		echo '<p>'; print "Nessun commento per questo prodotto"; echo '</p>';//echo '</td>' ;
 		echo '<br/>';
-	}
-	if(isset($_SESSION["login"])){//c'è qualcuno loggato
-    	if($contacommentoutente > 0){//se l'utente loggato ha fatto un commento lo stampo nel form poi modificabile
-    		echo '<form method="post" action="php/updatecommento.php?ido='.$arraydaticommento["Id_commento"].'">';
-    		echo '<input type="text" name="Comm" value="'; echo $arraydaticommento["Testo"]; echo ' " size="150">';
-			echo '<br/>';
-			echo '<input type="submit" name="submit" value="Modifica">';//qua fare update e non inserire
-			if(isset($_SESSION["inscom"])){
-				$inscom=$_SESSION["inscom"];
-				unset($_SESSION["inscom"]);
-				echo '<br/>';
-				if($inscom=="ok")
-					echo 'Inserimento avvenuto';
-				if($inscom=="okk")
-					echo 'Modifica avvenuta';
-			}
-    	}else{//l'utente non ha fatto aclucn commento
-			echo '<form method="post" action="php/inseriscicommento.php?ido='.$ido.'">';
-			echo '<input type="text" name="Comm" placeholder="Inserisci il tuo commento" size="150">';
-			echo '<br/>';
-			echo '<input type="submit" name="submit" value="Inserisci commento">';
 		}
-	}else{//non c'è nessuno loggato
-		echo '<input type="text" name="percommentare" readonly value="Devi effettuare il login per inserire un commento" size="150">';
-	}
-    ?>
+		if(isset($_SESSION["login"])){//c'è qualcuno loggato
+    		if($contacommentoutente > 0){//se l'utente loggato ha fatto un commento lo stampo nel form poi modificabile
+    			echo '<form method="post" action="php/updatecommento.php?ido='.$arraydaticommento["Id_commento"].'">';
+    			echo '<br/>Il tuo commento:<br/>';
+    			echo '<textarea rows="6"  cols="50" name="Comm">';
+    			echo $arraydaticommento["Testo"];
+				echo '</textarea>';
+				echo '<input type="submit" name="submit" value="Modifica">';//qua fare update e non inserire
+				if(isset($_SESSION["inscom"])){
+				$inscom=$_SESSION["inscom"];
+					unset($_SESSION["inscom"]);
+					echo '<br/>';
+					if($inscom=="ok")
+						echo 'Inserimento avvenuto';
+					if($inscom=="okk")
+						echo 'Modifica avvenuta';
+				}
+    		}else{//l'utente non ha fatto aclucn commento
+				echo '<form method="post" action="php/inseriscicommento.php?ido='.$ido.'">';
+				echo '<textarea rows="6" cols="50" name="Comm" placeholder="Inserisci il tuo commento"></textarea>';
+				echo '<br/>';
+				echo '<input type="submit" name="submit" value="Inserisci commento">';
+			}
+		}else{//non c'è nessuno loggato
+			echo '<input type="text" name="percommentare" readonly value="Devi effettuare il login per inserire un commento">';
+		}
+    	?>
+    </div>
 </div>
